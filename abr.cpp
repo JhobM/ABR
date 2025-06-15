284d5c3943b3ac77eb8063ad29965c30e4cc3456
7697c <76974955@continental.edu.pe>
2025-06-14 20:29:52 -0500

    Update abr.cpp

--- abr.cpp
+++ abr.cpp
@@ -6,7 +6,13 @@ struct nodo {
    nodo *izq;
    nodo *der;
 };
-
+nodo *nuevonodo(int valor) {
+   nodo *Nodo = new nodo();
+   Nodo->dato = valor;
+   Nodo->izq = NULL;
+   Nodo->der = NULL;
+   return Nodo;
+}
 
 int main()
 {
