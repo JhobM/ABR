449fea4dadb15f95e2d38fa8777bf059836c072a
7697c <76974955@continental.edu.pe>
2025-06-14 20:56:12 -0500

    Update abr.cpp

--- abr.cpp
+++ abr.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 
 struct nodo {
-   int dato;
-   nodo *izq;
-   nodo *der;
+	int dato;
+	nodo *izq;
+	nodo *der;
 };
+
 nodo *nuevonodo(int valor) {
-   nodo *Nodo = new nodo();
-   Nodo->dato = valor;
-   Nodo->izq = NULL;
-   Nodo->der = NULL;
-   return Nodo;
+        nodo *Nodo = new nodo();
+        Nodo->dato = valor;
+        Nodo->izq = NULL;
+        Nodo->der = NULL;
+        return Nodo;
 }
 
+nodo *insertar(nodo* raiz, int valor){
+	if(raiz == NULL){
+		return nuevonodo (valor);
+	}
+        if(valor < raiz->dato){
+		raiz->izq = insertar
+	}   
 int main()
 {
 	
