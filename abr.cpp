555cd569ee32070b181fe85833d30cb06666dd6f
7697c <76974955@continental.edu.pe>
2025-06-16 18:45:57 -0500

    Update abr.cpp

--- abr.cpp
+++ abr.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-struct nodo {
-	int dato;
-	nodo *izq;
-	nodo *der;
+struct Nodo {
+	string nombre;
+        int puntuacion;
+	nodo* izq;
+	nodo* der;
 };
 
-nodo *nuevonodo(int valor) {
-        nodo *Nodo = new nodo();
-        Nodo->dato = valor;
-        Nodo->izq = NULL;
-        Nodo->der = NULL;
-        return Nodo;
+nodo* crearNodo(string nombre, int puntuacion) {
+        Nodo* nuevo = new Nodo();
+        nuevo->nombre = nombre;
+        nuevo->puntuacion = puntuacion;
+        nuevo->izq = nuevo->der = NULL;
+        return nuevo;
 }
 
 nodo *insertar(nodo* raiz, int valor){
